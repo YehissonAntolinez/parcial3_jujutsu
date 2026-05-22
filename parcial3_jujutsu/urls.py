daf6d5b1da3e412f631bd5dba6642584cf540b6d
YehissonAntolinez <132116319+YehissonAntolinez@users.noreply.github.com>
2026-05-22 17:45:25 -0500

    Update urls.py

--- parcial3_jujutsu/urls.py
+++ parcial3_jujutsu/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('jujutsu.urls')),  # Esto le dice a Django que busque las rutas dentro de la app
+    path('api/', include('jujutsu.urls')),  
 ]
